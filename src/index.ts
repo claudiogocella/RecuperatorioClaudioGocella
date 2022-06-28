82a4b826fd7845730458727ef3a477af7efab4c4
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-06-28 17:01:37 -0300

    cambio en console log intento mejorar la legibilidad de lo que muestra por pantalla

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// Examen Recuperatorio con fecha de entrega el dia 28/06
 let cantidadDeProductos: number = Number(
   prompt("Ingrese la cantidad de productos que compró ")
 );
+
 let nombresDeProductos: string[] = new Array(cantidadDeProductos);
-let cantidadDeEseProducto: number[] = new Array(cantidadDeProductos);
+let cantidadDeProducto: number[] = new Array(cantidadDeProductos);
 let precioUnitario: number[] = new Array(cantidadDeProductos);
 let sumaTotal: number = 0;
 
@@ -12,9 +12,10 @@ function cargarProductos(arreglo: string[], longitud: number) {
 
   for (i = 0; i < longitud; i++) {
     arreglo[i] = prompt("Ingrese el nombre del " + (i + 1) + "º producto");
-    //    console.log(arreglo[i]);
+        console.log(arreglo[i]);
   }
 }
+
 function cargarLaCantidadProductos(arreglo: number[], longitud: number) {
   let i: number;
   for (i = 0; i < longitud; i++) {
@@ -31,10 +32,11 @@ function cargarPrecioUnitarioDelProducto(arreglo: number[], longitud: number) {
     );
   }
 }
+
 function mostrarProductos(arreglo: number[], longitud: number) {
   let i: number;
   for (i = 0; i < longitud; i++) {
-    //    console.log(arreglo[i]);
+        console.log(arreglo[i]);
   }
 }
 
@@ -51,26 +53,52 @@ function calcularCompra(
 }
 
 cargarProductos(nombresDeProductos, cantidadDeProductos);
-cargarLaCantidadProductos(cantidadDeEseProducto, cantidadDeProductos);
+cargarLaCantidadProductos(cantidadDeProducto, cantidadDeProductos);
 cargarPrecioUnitarioDelProducto(precioUnitario, cantidadDeProductos);
-console.log("Los Productos comprados son: ");
+console.log("Usted compró los siguientes productos: ");
+
 mostrarProductos(nombresDeProductos, cantidadDeProductos);
-console.log("Las cantidades de los productos comprados: ");
-mostrarProductos(cantidadDeEseProducto, cantidadDeProductos);
-console.log("El Precio de los productos comprados: ");
+console.log("Las cantidades de los productos comprados respectivamente son: ");
+mostrarProductos(cantidadDeProducto, cantidadDeProductos);
+console.log("El Precio unitario de los productos comprados respectivamente es: ");
 mostrarProductos(precioUnitario, cantidadDeProductos);
 console.log(
-  "El costo total de la compra es: " +
-    calcularCompra(cantidadDeEseProducto, precioUnitario, cantidadDeProductos)
+  "El costo total de su compra es: " +
+    calcularCompra(cantidadDeProducto, precioUnitario, cantidadDeProductos)
 );
+
+/*
 if (sumaTotal < 1000) {
   console.log(
-    "Su compra debe ser mayor a $1000 no esta participando en ningun sorteo"
+    "Usted no supera el mínimo de compra para participar del sorteo"
   );
-} else if (sumaTotal > 1000 && sumaTotal < 2000) {
-  console.log("Usted esta participando por un TV Led");
-} else if (sumaTotal > 2000 && sumaTotal < 3000) {
-  console.log("Usted esta participando por una moto 0Km");
-} else if (sumaTotal > 3000) {
-  console.log("Usted esta participando por un Auto 0Km");
+
+  } else if (sumaTotal > 1000 && sumaTotal < 2000) {
+  console.log("Con su compra, usted esta participando por un TV Led");
+
+  } else if (sumaTotal > 2000 && sumaTotal < 3000) {
+  console.log("Con su compra, usted esta participando por una Moto 0Km");
+
+  } else if (sumaTotal > 3000) {
+  console.log("Con su compra, usted esta participando por un Auto 0Km");
 }
+*/
+
+
+
+if (sumaTotal >= 1000 && total < 2000) {
+  console.log( 
+    "Felicidades, participa en el sorteo de un tv led"
+    );
+  } else if (sumaTotal >= 2000 && total < 3000) {
+    console.log (
+      "Felicidades, participa en el sorteo de una moto 0 Km");
+
+  } else if (sumaTotal >= 3000) {
+    console.log (
+      "Felicidades, participa en el sorteo de un auto 0 Km)";
+
+  } else {
+    console.log (
+      "Gracias por su compra");
+  }
